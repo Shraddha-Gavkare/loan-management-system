3158859887d1475c112c9f93637b8ae3f5f24307
Shraddha-Gavkare <112618249+Shraddha-Gavkare@users.noreply.github.com>
2025-04-09 05:05:20 -0600

    Update DBConnUtil.java

--- src/hex/loan/util/DBConnUtil.java
+++ src/hex/loan/util/DBConnUtil.java
@@ -1,29 +1,33 @@
 package hex.loan.util;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class DBConnUtil {
 
-    public static Connection getDBConn() {
+    private static final String fileName = "db/db.properties"; // relative path from project root
+
+    public static Connection getDbConnection() {
         Connection conn = null;
+        String connStr = null;
+
         try {
-            Properties props = DBPropertyUtil.getPropertyString("db/db.properties");
-            String driver = props.getProperty("driver");
-            String url = props.getProperty("url");
-            String username = props.getProperty("username");
-            String password = props.getProperty("password");
-
-            Class.forName(driver);
-            conn = DriverManager.getConnection(url, username, password);
-
-        } catch (ClassNotFoundException | SQLException e) {
-            System.out.println("Database connection failed.");
+            connStr = DBPropertyUtil.getConnectionString(fileName);
+        } catch (IOException e) {
+            System.out.println("Connection String Creation Failed");
             e.printStackTrace();
         }
 
+        if (connStr != null) {
+            try {
+                conn = DriverManager.getConnection(connStr);
+            } catch (SQLException e) {
+                System.out.println("Error While Establishing DB Connection...");
+                e.printStackTrace();
+            }
+        }
         return conn;
     }
 }
